478167bb70b62235b967cfdb4aabc96318d7a4e8
Carlos Garcia <agzsoftsi@gmail.com>
2022-03-07 23:21:31 -0500

    first change

---
+++ todo-list/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <Header />
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-list/src/components/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+import React from 'react'
+
+function Header() {
+  return (
+    <div>My Title</div>
+  )
+}
+
+export default Header
